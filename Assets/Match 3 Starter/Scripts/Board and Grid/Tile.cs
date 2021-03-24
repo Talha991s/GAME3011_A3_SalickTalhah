bacd41fd60fa7678b891eff31e295a59e71bb6e0
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-03-24 01:36:08 -0700

    Added Swapping

---
+++ Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -0,0 +1,70 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour {
+	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Tile previousSelected = null;
+
+	private SpriteRenderer render;
+	private bool isSelected = false;
+
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	void Awake() {
+		render = GetComponent<SpriteRenderer>();
+    }
+
+	private void Select() {
+		isSelected = true;
+		render.color = selectedColor;
+		previousSelected = gameObject.GetComponent<Tile>();
+		SFXManager.instance.PlaySFX(Clip.Select);
+	}
+
+	private void Deselect() {
+		isSelected = false;
+		render.color = Color.white;
+		previousSelected = null;
+	}
+
+    private void OnMouseDown()
+    {
+        if(render.sprite == null || BoardManager.instance.IsShifting)
+        {
+			return;
+        }
+
+		if(isSelected)
+        {
+			Deselect();
+        }
+        else
+        {
+			if(previousSelected == null)
+            {
+				Select();
+            }
+			else
+            {
+				SwapSprite(previousSelected.render);
+				previousSelected.Deselect();
+            }
+        }
+    }
+
+	public void SwapSprite(SpriteRenderer render2)
+    {
+		if(render.sprite == render2.sprite)
+        {
+			return;
+        }
+
+		Sprite tempSprite = render2.sprite;
+		render2.sprite = render.sprite;
+		render.sprite = tempSprite;
+		SFXManager.instance.PlaySFX(Clip.Swap);
+    }
+
+}
